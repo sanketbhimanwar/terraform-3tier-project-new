08db8c7665dba55b407ad847d2d28f96077b83a3
sanket <111006643+sanketbhimanwar@users.noreply.github.com>
2026-06-14 16:51:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "reya-tfstate-bucket-123"
+    bucket         = "sanket-tfstate-bucket-26"
     key            = "3tier/prod/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile =  true
